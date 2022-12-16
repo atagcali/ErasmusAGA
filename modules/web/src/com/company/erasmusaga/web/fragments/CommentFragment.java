8cae6f34439f37ed4cd75cd978aeeb237971cebc
Alp Tuğrul Ağçalı <agcalialp@gmail.com>
2022-12-16 15:16:54 +0300

    feature/Application_List_Ekranının_Oluşturulması

--- modules/web/src/com/company/erasmusaga/web/fragments/CommentFragment.java
+++ modules/web/src/com/company/erasmusaga/web/fragments/CommentFragment.java
@@ -3,6 +3,7 @@ package com.company.erasmusaga.web.fragments;
 import com.company.erasmusaga.entity.Comment;
 import com.haulmont.cuba.core.entity.FileDescriptor;
 import com.haulmont.cuba.gui.Fragments;
+import com.haulmont.cuba.gui.components.Label;
 import com.haulmont.cuba.gui.components.TextArea;
 import com.haulmont.cuba.gui.components.VBoxLayout;
 import com.haulmont.cuba.gui.screen.ScreenFragment;
@@ -23,15 +24,19 @@ public class CommentFragment extends ScreenFragment {
     private Fragments fragments;
     @Inject
     private VBoxLayout Vbox;
+    @Inject
+    private Label<String> userLbl;
 
     public void setComment(Comment comment){ this.comment = comment; }
 
     @Subscribe
     public void onInit(InitEvent event) {
+        userLbl.setValue(comment.getUser().getName() + " Tarafından " + comment.getCreateTs().toString() + " tarihinde " + " atıldı.");
         commentTxt.setValue(comment.getContent());
         for (FileDescriptor fileDescriptor : comment.getDocuments()) {
             FileFragment fileFragment = fragments.create(this, FileFragment.class);
             fileFragment.setFileDescriptor(fileDescriptor);
+            fileFragment.setAdd(false);
             Vbox.add(fileFragment.getFragment());
         }
     }
